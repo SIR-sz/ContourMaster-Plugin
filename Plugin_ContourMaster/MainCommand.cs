b41b8572ea2441dbd76a9eaa54c0e0a24813f603
lsz <lsz@LSZ-PC>
2026-01-16 22:48:30 +0800

    添加项目文件。

---
+++ Plugin_ContourMaster/MainCommand.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows;
+using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms.Integration;
+
+// 注册命令类
+[assembly: CommandClass(typeof(Plugin_ContourMaster.MainCommand))]
+[assembly: ExtensionApplication(typeof(Plugin_ContourMaster.MainCommand))]
+
+namespace Plugin_ContourMaster
+{
+    public class MainCommand : IExtensionApplication
+    {
+        static PaletteSet _ps = null;
+
+        public void Initialize()
+        {
+#if STANDALONE
+            // 独立版在加载时可以给用户一点反馈
+            var ed = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
+            ed.WriteMessage("\n>>> 像素轮廓专家 (独立定制版) 加载成功！");
+#endif
+        }
+
+        public void Terminate() { }
+
+        // 在 MainCommand.cs 的 ShowContourPanel 方法中更新：
+        [CommandMethod("PXQLK_PANEL")]
+        public void ShowContourPanel()
+        {
+            if (_ps == null)
+            {
+                Guid myPaletteId = new Guid("F3A8E9B2-C12D-4C11-8D9A-2B3C4D5E6F7A");
+                _ps = new PaletteSet("像素轮廓工具箱", myPaletteId);
+                _ps.MinimumSize = new System.Drawing.Size(300, 500);
+                _ps.Style = PaletteSetStyles.ShowPropertiesMenu | PaletteSetStyles.ShowCloseButton;
+
+                // 加载刚才创建的 WPF 控件
+                var control = new ContourMainControl();
+                ElementHost host = new ElementHost { Child = control, Dock = System.Windows.Forms.DockStyle.Fill };
+                _ps.Add("算法参数", host);
+            }
+            _ps.Visible = true;
+        }
+    }
+}
---
+++ Plugin_ContourMaster/Models.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Plugin_ContourMaster
+{
+    [Obfuscation(Feature = "renaming", Exclude = true)]
+    public class ContourSettings
+    {
+        public double Threshold { get; set; } = 128.0; // 灰度阈值
+        public bool IsInverse { get; set; } = false;   // 是否反向选择
+        public double SimplifyTolerance { get; set; } = 0.5; // 曲线简化公差
+        public string LayerName { get; set; } = "Contour_Result"; // 结果图层
+
+    }
+
+}
